8c6cb3527751dfca71576027468e800c2a485dbd
Svyatoslav228-eng <slavik20012008@gmail.com>
2024-08-26 16:29:58 +0300

    reactionssss

--- front/src/app/questions/all topics/topic.jsx
+++ front/src/app/questions/all topics/topic.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 
 const Topic = ({ id, title, owner, created_at, views }) => {
+	const [questions, setQuestions] = useState([])
+
 	const handleClick = async () => {
 		try {
 			const response = await axios.put(
@@ -13,7 +15,27 @@ const Topic = ({ id, title, owner, created_at, views }) => {
 			console.error('Error incrementing views:', error)
 		}
 	}
-
+	useEffect(() => {
+		fetchQuestions()
+	}, [])
+	const fetchQuestions = async () => {
+		try {
+			const response = await axios.get(
+				`http://srv509462.hstgr.cloud:8001/api/messages/by-topic-title/${encodeURIComponent(
+					title
+				)}/questions/`
+			)
+			if (response.status === 200) {
+				setQuestions(response.data)
+			} else {
+				console.error('Failed to fetch the questions of the topic.')
+			}
+		} catch (error) {
+			console.error('Error fetching questions: ', error)
+		} finally {
+			setLoading(false)
+		}
+	}
 	return (
 		<div>
 			<div className='bg-white shadow-md rounded p-4 border border-gray-300 mb-2'>
@@ -32,6 +54,7 @@ const Topic = ({ id, title, owner, created_at, views }) => {
 
 				<div>{owner}</div>
 				<div>{views} views</div>
+				<div>{questions.reaction} reactions</div>
 				<div>{created_at}</div>
 			</div>
 		</div>
