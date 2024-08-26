8599a767d40a809ac222d5a862760df891820d4c
Svyatoslav228-eng <slavik20012008@gmail.com>
2024-08-26 15:29:32 +0300

    answr

--- front/src/app/topic/answers/answers.jsx
+++ front/src/app/topic/answers/answers.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import Answer from './Answer'
+import Answer from './answer'
 
 const Answers = ({ title }) => {
 	const [answers, setAnswers] = useState([])
